665395934c2fe153cfbe641f85a27449f48fc81e
dongju <131073398+Dongjuice@users.noreply.github.com>
2023-10-11 18:58:06 +0900

    [Add]: 프로젝트 파일 생성

---
+++ DearMyFriend/DearMyFriend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DearMyFriend
+//
+//  Created by Macbook on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
