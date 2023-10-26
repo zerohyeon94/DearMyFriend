a20a46bc8f15d146ff74089252eadb31e00c1b55
1hanjung <jungillhan@naver.com>
2023-10-26 15:37:45 +0900

    [FEAT]:userinfoUI 수정

--- DearMyFriend/DearMyFriend/ViewController/Login/UserInfoViewController.swift
+++ DearMyFriend/DearMyFriend/ViewController/Login/UserInfoViewController.swift
@@ -1,8 +1,22 @@
 import UIKit
 
-class UserInfoViewController: UIViewController {
+class UserInfoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     weak var mainViewController: TabBarController?
+    
+    private lazy var profileImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 50
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "defaultProfile") // 디폴트 이미지 설정
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(profileImageTapped))
+        imageView.addGestureRecognizer(tapGesture)
+        return imageView
+    }()
     
     private lazy var nextButton: UIButton = {
         let button = UIButton(type: .custom)
@@ -20,16 +34,41 @@ class UserInfoViewController: UIViewController {
         view.backgroundColor = .white
         makeUI()
     }
-
+    
     func makeUI() {
+        view.addSubview(profileImageView)
         view.addSubview(nextButton)
         
         NSLayoutConstraint.activate([
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200), // centerYAnchor에 constant 추가
+            profileImageView.widthAnchor.constraint(equalToConstant: 100),
+            profileImageView.heightAnchor.constraint(equalToConstant: 100),
+
+            
             nextButton.widthAnchor.constraint(equalToConstant: 120),
             nextButton.heightAnchor.constraint(equalToConstant: 45),
             nextButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
             nextButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
         ])
+    }
+    
+    @objc func profileImageTapped() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImageView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
     @objc func nextButtonTapped() {
