9debfa0df1de9e5e69475633ec867ef8d54f045d
zerohyeon <joyh0808@gmail.com>
2023-11-03 11:29:58 +0900

    [Feat]: My Profile View의 Title View 구현

---
+++ DearMyFriend/DearMyFriend/View/MyProfile/MyProfileTitleView.swift
@@ -0,0 +1,96 @@
+import Foundation
+import UIKit
+
+protocol MyProfileTitleViewDelegate: AnyObject {
+    func settingButtonTapped()
+}
+
+class MyProfileTitleView: UIView {
+    
+    // MARK: Properties
+    // Label
+    let titleLabelSize: CGFloat = 20
+    
+    // Button
+    let buttonSize: CGFloat = 30
+    let buttonImage: String = "gearshape"
+    let buttonColor: UIColor = .black
+    
+    var delegate: MyProfileTitleViewDelegate?
+    
+    lazy var myPageTitleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont(name: "SpoqaHanSansNeo-Bold", size: titleLabelSize)
+        label.text = "Me & My Friend"
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    lazy var settingButton: UIButton = {
+        let button = UIButton()
+        
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: buttonSize, weight: .light)
+        let image = UIImage(systemName: buttonImage, withConfiguration: imageConfig)
+        
+        button.setImage(image, for: .normal)
+        button.tintColor = buttonColor
+        
+        button.addTarget(self, action: #selector(settingButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    // MARK: Initalizers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Configure
+    private func configure() {
+        setUI()
+        setConstraint()
+    }
+    
+    private func setUI(){
+        [myPageTitleLabel, settingButton].forEach { view in
+            addSubview(view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func setConstraint() {
+        setNicknameLabelConstraint()
+        setAddPostButtonConstraint()
+    }
+    
+    private func setNicknameLabelConstraint() {
+        NSLayoutConstraint.activate([
+            myPageTitleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            myPageTitleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            myPageTitleLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            myPageTitleLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setAddPostButtonConstraint() {
+        NSLayoutConstraint.activate([
+            settingButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            settingButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            settingButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    // MARK: Action
+    @objc func settingButtonTapped() {
+        print("설정 창 실행")
+        delegate?.settingButtonTapped()
+    }
+}
--- DearMyFriend/DearMyFriend/ViewController/Entry/MyProfile/MyViewController.swift
+++ DearMyFriend/DearMyFriend/ViewController/Entry/MyProfile/MyViewController.swift
@@ -2,21 +2,40 @@ import UIKit
 
 class MyViewController: UIViewController {
 
+    // MARK: Properties
+    let myPrfileTitleView: MyProfileTitleView = .init(frame: .zero)
+    let myPrfileTitleViewHeight: CGFloat = 50
+    
+    // TableView
+    private let feedTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        
+        configure()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        // NavigationBar 숨김.
+        navigationController?.isNavigationBarHidden = true
     }
-    */
-
+    
+    // MARK: Configure
+    private func configure() {
+        view.backgroundColor = .white
+        setupMyProfileTitleView()
+    }
+    
+    private func setupMyProfileTitleView() {
+        view.addSubview(myPrfileTitleView)
+        myPrfileTitleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            myPrfileTitleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            myPrfileTitleView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            myPrfileTitleView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            myPrfileTitleView.heightAnchor.constraint(equalToConstant: myPrfileTitleViewHeight)
+        ])
+    }
 }
